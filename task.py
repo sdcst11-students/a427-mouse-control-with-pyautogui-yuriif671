b3d83d0c802fe1c0dbfedb57d5ed064e6841cce3
yuriif671 <yuriif767@deltalearns.ca>
2023-11-24 13:23:44 -0800

    Done and used a different maze

---
+++ task.py
@@ -0,0 +1,23 @@
+import pyautogui 
+import time 
+
+"""
+this one is to be run on a 1980x1920 monitor with VScode in windowed mode and with 'made-game-for-kids...' opened to the right in split screen and this file opened to the left
+"""
+
+pyautogui.moveTo(1130, 210, 1)
+pyautogui.moveTo(1130, 240, 1)
+pyautogui.moveTo(1200, 240, 1)
+pyautogui.moveTo(1200, 310, 1)
+pyautogui.moveTo(1160, 310, 1)
+pyautogui.moveTo(1160, 275, 1)
+pyautogui.moveTo(1130, 275, 1)
+pyautogui.moveTo(1130, 310, 1)
+pyautogui.moveTo(1050, 310, 1)
+pyautogui.moveTo(1050, 375, 1)
+pyautogui.moveTo(1100, 375, 1)
+pyautogui.moveTo(1100, 350, 1)
+pyautogui.moveTo(1170, 350, 1)
+pyautogui.moveTo(1170, 375, 1)
+pyautogui.moveTo(1120, 375, 1)
+pyautogui.moveTo(1120, 395, 1)
